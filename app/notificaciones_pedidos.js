7a357d03244951732e4856e001d1b4e81509e53e
liam <liambialy2@gmail.com>
2024-05-27 20:03:16 -0300

    Closes #3 - se creo el archivo notificaciones_pedidos.js

--- app/notificaciones_pedidos.js
+++ app/notificaciones_pedidos.js
@@ -1,2 +1,2 @@
 // Este es el archivo que permite al bibliotecario registrar los pedidos de libros por parte de los alumnos
-console.log("archivo para manejar las notificaciones de los pedidos de libros...")
+console.log("archivo para manejar las notificaciones de los pedidos de libros....")
